4dcb3d28df46e8696d586b9697c703d557755dee
Admin Coachtech <admin@coachtech.pro>
2020-11-28 01:51:54 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './img/GeeksCapitalLogo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>GeeksCapital.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
